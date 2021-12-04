147810b8ce20d0a734697fece3d700d8ba7904a7
Wojciech Borkowski <wojtek.borkowski@gmail.com>
2021-12-04 11:06:42 +0100

    Add promt for player move in script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
 
+// 1: kamień, 2: papier, 3: nożyce.
+
 let computerMove = `kamień`;
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 if(randomNumber == 2){
-    computerMove = 'pamier'
+    computerMove = 'papier'
 }
 else if(randomNumber == 3){
-    computerMove = 'nozyce'
+    computerMove = 'nożyce'
 }
 console.log(randomNumber)
-printMessage(computerMove);
+printMessage('Ruch komputera to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+let playerMove = 'nieznany ruch';
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+else if(playerInput == '2'){
+    playerMove = 'papier';
+}
+else if(playerInput == '3'){
+    playerMove = 'nożyce';
+}
+printMessage('Mój ruch to: ' + playerMove);

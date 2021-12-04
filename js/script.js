3fd2957f38a88d92f748f571f28eb7a44f64ac44
Wojciech Borkowski <wojtek.borkowski@gmail.com>
2021-12-04 13:06:23 +0100

    Add buttons in index.html, Add playGame function in script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
 
 // 1: kamień, 2: papier, 3: nożyce.
 
-let randomNumber = Math.floor(Math.random() * 3 + 1);
-let computerMove = getMoveName(randomNumber);
-printMessage('Ruch komputera to: ' + computerMove);
+function playGame(playerInput){
+    clearMessages();
+    let playerMove = getMoveName(playerInput);
+    printMessage('Twój ruch to: ' + playerMove);
 
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-let playerMove = getMoveName(playerInput);
-printMessage('Twój ruch to: ' + playerMove);
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+    let computerMove = getMoveName(randomNumber);
+    printMessage('Ruch komputera to: ' + computerMove);
 
-printMessage(displayResult(computerMove, playerMove));
+    printMessage(displayResult(computerMove, playerMove));
+}
+document.getElementById('play-rock').addEventListener('click', function(){
+    playGame("1")
+  });
+document.getElementById('play-paper').addEventListener('click', function(){
+    playGame("2")
+  });
+document.getElementById('play-scissors').addEventListener('click', function(){
+    playGame("3")
+  });

6b95ae831e201a428b9c45701d47260e5a16e1c7
Wojciech Borkowski <wojtek.borkowski@gmail.com>
2021-12-13 21:12:54 +0100

    Change all functions to anonymous, change let variables declarations to const

--- js/functions.js
+++ js/functions.js
@@ -1,72 +1,57 @@
-
 // 1: kamień, 2: papier, 3: nożyce.
-function getMoveName(argMoveId){
-    if(argMoveId == 1){
-      return 'kamień';
-    }
-    else if(argMoveId == 2){
-        return 'papier';
-    }
-    else if(argMoveId == 3){
-        return 'nożyce';
-    }
-    else {
-      printMessage('Nie znam ruchu o id ' + argMoveId + '.');
-      return 'nieznany ruch';
-    }
-  }
+const getMoveName = function (argMoveId) {
+   if (argMoveId == 1) {
+      return "kamień";
+   } else if (argMoveId == 2) {
+      return "papier";
+   } else if (argMoveId == 3) {
+      return "nożyce";
+   } else {
+      printMessage("Nie znam ruchu o id " + argMoveId + ".");
+      return "nieznany ruch";
+   }
+};
 
-function displayResult(argComputerMove, argPlayerMove){
-    if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
-        return 'Wygrałeś!'
-      }
-    else if( argComputerMove == 'kamień' && argPlayerMove == 'nożyce'){
-        return 'Przegrałeś'
-      }
-    else if( argComputerMove == 'kamień' && argPlayerMove == 'kamień'){
-        return 'Remis'
-      }
-    else if( argComputerMove == 'papier' && argPlayerMove == 'papier'){
-        return 'Remis'
-      }
-    else if( argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
-        return 'Wygrałeś!'
-      }
-    else if( argComputerMove == 'papier' && argPlayerMove == 'kamień'){
-        return 'Przegrałeś'
-      }
-    else if( argComputerMove == 'nożyce' && argPlayerMove == 'papier'){
-        return 'Przegrałeś'
-    }
-    else if( argComputerMove == 'nożyce' && argPlayerMove == 'nożyce'){
-        return 'Remis'
-    }
-    else if( argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
-        return 'Wygrałeś!'
-      }
-    else{
-        return 'Błędne wartości'
-    }
-}
+const displayResult = function (argComputerMove, argPlayerMove) {
+   if (argComputerMove == "kamień" && argPlayerMove == "papier") {
+      return "Wygrałeś!";
+   } else if (argComputerMove == "kamień" && argPlayerMove == "nożyce") {
+      return "Przegrałeś";
+   } else if (argComputerMove == "kamień" && argPlayerMove == "kamień") {
+      return "Remis";
+   } else if (argComputerMove == "papier" && argPlayerMove == "papier") {
+      return "Remis";
+   } else if (argComputerMove == "papier" && argPlayerMove == "nożyce") {
+      return "Wygrałeś!";
+   } else if (argComputerMove == "papier" && argPlayerMove == "kamień") {
+      return "Przegrałeś";
+   } else if (argComputerMove == "nożyce" && argPlayerMove == "papier") {
+      return "Przegrałeś";
+   } else if (argComputerMove == "nożyce" && argPlayerMove == "nożyce") {
+      return "Remis";
+   } else if (argComputerMove == "nożyce" && argPlayerMove == "kamień") {
+      return "Wygrałeś!";
+   } else {
+      return "Błędne wartości";
+   }
+};
 
+const printMessage = function (element, msg) {
+   let div = document.createElement("div");
+   div.innerHTML = msg;
+   element.appendChild(div);
+};
 
-function printMessage(element, msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	element.appendChild(div);
-}
-
-function clearMessages(){
-	document.getElementById('result').innerHTML = '';
-	document.getElementById('computer-input').innerHTML = '';
-	document.getElementById('player-input').innerHTML = '';
-	document.getElementById('counter').innerHTML = '';
-    document.getElementById('computer-rock').style.display = "none";
-    document.getElementById('computer-paper').style.display = "none";
-    document.getElementById('computer-scissors').style.display = "none";
-    document.getElementById('player-rock').style.backgroundColor = "#81b29a";
-    document.getElementById('player-paper').style.backgroundColor = "#81b29a";
-    document.getElementById('player-scissors').style.backgroundColor = "#81b29a";
-    document.getElementById('result').style.color = "black";
-}
-
+const clearMessages = function () {
+   document.getElementById("result").innerHTML = "";
+   document.getElementById("computer-input").innerHTML = "";
+   document.getElementById("player-input").innerHTML = "";
+   document.getElementById("counter").innerHTML = "";
+   document.getElementById("computer-rock").style.display = "none";
+   document.getElementById("computer-paper").style.display = "none";
+   document.getElementById("computer-scissors").style.display = "none";
+   document.getElementById("player-rock").style.backgroundColor = "#81b29a";
+   document.getElementById("player-paper").style.backgroundColor = "#81b29a";
+   document.getElementById("player-scissors").style.backgroundColor = "#81b29a";
+   document.getElementById("result").style.color = "black";
+};
--- js/script.js
+++ js/script.js
@@ -1,44 +1,38 @@
-
 // 1: kamień, 2: papier, 3: nożyce.
-
 let counter = 0;
-function playGame(playerInput){
-    clearMessages();
-    let playerMove = getMoveName(playerInput);
-    let randomNumber = Math.floor(Math.random() * 3 + 1);
-    let computerMove = getMoveName(randomNumber);
 
-    if(computerMove == 'kamień'){
-        document.getElementById('computer-rock').style.display = "inline";
-    }
-    else if(computerMove == 'papier'){
-        document.getElementById('computer-paper').style.display = "inline";
-    }
-    else if(computerMove == 'nożyce'){
-        document.getElementById('computer-scissors').style.display = "inline";
-    }
-    let resultMsg = displayResult(computerMove, playerMove);
-    printMessage(document.getElementById('result'), resultMsg);
-    if(resultMsg == 'Wygrałeś!'){
-        document.getElementById('result').style.color = "green";
-        counter++;
-        console.log(counter);
-    }
-    else if(resultMsg == 'Przegrałeś'){
-        document.getElementById('result').style.color = "red";
-    }
-    printMessage(document.getElementById('counter'), 'Wygrane: ' + counter);
-}
+const playGame = function (playerInput) {
+   clearMessages();
+   const playerMove = getMoveName(playerInput);
+   const randomNumber = Math.floor(Math.random() * 3 + 1);
+   const computerMove = getMoveName(randomNumber);
+   if (computerMove == "kamień") {
+      document.getElementById("computer-rock").style.display = "inline";
+   } else if (computerMove == "papier") {
+      document.getElementById("computer-paper").style.display = "inline";
+   } else if (computerMove == "nożyce") {
+      document.getElementById("computer-scissors").style.display = "inline";
+   }
+   const resultMsg = displayResult(computerMove, playerMove);
+   printMessage(document.getElementById("result"), resultMsg);
+   if (resultMsg == "Wygrałeś!") {
+      document.getElementById("result").style.color = "green";
+      counter++;
+   } else if (resultMsg == "Przegrałeś") {
+      document.getElementById("result").style.color = "red";
+   }
+   printMessage(document.getElementById("counter"), "Wygrane: " + counter);
+};
 
-document.getElementById('player-rock').addEventListener('click', function(){
-    playGame("1")
-    document.getElementById('player-rock').style.backgroundColor = "#168aad";
-  });
-document.getElementById('player-paper').addEventListener('click', function(){
-    playGame("2")
-    document.getElementById('player-paper').style.backgroundColor = "#168aad";
-  });
-document.getElementById('player-scissors').addEventListener('click', function(){
-    playGame("3")
-    document.getElementById('player-scissors').style.backgroundColor = "#168aad";
-  });
+document.getElementById("player-rock").addEventListener("click", function () {
+   playGame("1");
+   document.getElementById("player-rock").style.backgroundColor = "#168aad";
+});
+document.getElementById("player-paper").addEventListener("click", function () {
+   playGame("2");
+   document.getElementById("player-paper").style.backgroundColor = "#168aad";
+});
+document.getElementById("player-scissors").addEventListener("click", function () {
+   playGame("3");
+   document.getElementById("player-scissors").style.backgroundColor = "#168aad";
+});
